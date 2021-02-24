2ef575108caab90038013b54628608ab0a7cffb1
HabiburRahmanAnik <habib264521@gmail.com>
2021-02-24 09:39:23 +0600

    php task 7

---
+++ MID_LAB_TASK_PHP_SESSION_07/home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Public Home</title>
+</head>
+<body>
+
+	<fieldset>
+		
+	<table align="center" width="100%">
+		<tr height="60px">
+			<td width="50px">
+				<img src="images/logo.png" width="130px">
+			</td>
+
+			<td align="right">
+			<a href="home.html">Home</a>|
+			<a href="login.html">Login</a>|
+			<a href="registration.html">Registration</a>
+			</td>
+		</tr>
+	</table>
+	<hr>
+	<table>
+		<tr height="200px">
+			<td colspan="2"><h2>Welcome xCompany</h2></td>
+		</tr>
+	</table>
+	<hr>
+	<p align="center">Copyright &copy 2017</p>
+	</fieldset>
+	
+</body>
+</html>
